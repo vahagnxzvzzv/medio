808ec16dd39b7996799e314eea265e701f05065d
Vahagn <vahagngasparyan5@gmail.com>
2024-01-30 13:04:54 +0400

    commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { Sidebar, Videos } from './'
+import { fetchFromApi } from '../utils/fetchFromApi'
+
+function Feed() {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+      // console.log(videos);
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+        <Box
+          sx={{ height: { sx: 'auto', md: '92vh' },borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}  }}
+        >
+          <Sidebar 
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />  
+
+          <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+              Copyright 2024 Medio
+          </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+          <Typography variant='h4' fontWeight='bold' mb={2} sx={{
+            color: 'white'
+          }}> 
+              {selectedCategory}
+              <span style={{ color: '#F31503' }}>videos</span>
+          </Typography>
+
+          <Videos  
+              videos={videos}
+          />
+        </Box>
+    </Stack>
+  )
+}
+
+export default Feed
